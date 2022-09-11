5bf99bac1fd58bb31eab01b5f731a9ec3771f8aa
Arsalan Cheema <arsalanc.v2@gmail.com>
2022-09-11 21:49:42 +0800

    update protocol prelude

--- cdp/prelude/CDP/Protocol.hs
+++ cdp/prelude/CDP/Protocol.hs
@@ -1,12 +1,39 @@
 {-# LANGUAGE OverloadedStrings, RecordWildCards, TupleSections #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE DeriveGeneric #-}
+ {-# LANGUAGE ScopedTypeVariables #-}
+ {-# LANGUAGE FlexibleContexts #-}
+ {-# LANGUAGE MultiParamTypeClasses #-}
+ {-# LANGUAGE FlexibleInstances #-}
+ {-# LANGUAGE DeriveGeneric #-}
 
-module CDP.Protocol (module CDP.Protocol) where
+ module CDP.Protocol where
 
-import Data.Proxy
+
+ import           Control.Applicative  ((<$>))
+ import           Control.Monad
+ import           Control.Monad.Loops
+ import           Control.Monad.Trans  (liftIO)
+ import qualified Data.Map             as M
+ import           Data.Maybe          
+ import Data.Functor.Identity
+ import Data.String
+ import qualified Data.Text as T
+ import qualified Data.List as List
+ import qualified Data.Text.IO         as TI
+ import qualified Data.Vector          as V
+ import Data.Aeson.Types (Parser(..))
+ import           Data.Aeson           (FromJSON (..), ToJSON (..), (.:), (.:?), (.=), (.!=), (.:!))
+ import qualified Data.Aeson           as A
+ import qualified Network.HTTP.Simple as Http
+ import qualified Network.URI          as Uri
+ import qualified Network.WebSockets as WS
+ import Control.Concurrent
+ import qualified Text.Casing as C
+ import qualified Data.ByteString.Lazy as BS
+ import qualified Data.Map as Map
+ import Data.Proxy
+ import System.Random
+ import GHC.Generics
+ import Data.Char
+ import Data.Default
 
 import CDP.Runtime

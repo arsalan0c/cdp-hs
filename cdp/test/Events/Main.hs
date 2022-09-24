ba76dd5921fc0d155ff15d482db33dd0dc3c2241
Arsalan Cheema <arsalan0c@gmail.com>
2022-09-25 01:43:12 +0800

    Fix test and comment it out (#26)
    * fix test

--- cdp/test/Events/Main.hs
+++ cdp/test/Events/Main.hs
@@ -9,31 +9,33 @@ import Control.Monad
 import Control.Concurrent
 import Data.Default
 
-import qualified CDP as CDP
-import qualified CDP.Internal.Runtime as R
+import CDP as CDP
+import CDP.Handle
+import CDP.Internal.Runtime
 
-prop_page_frame :: Property
-prop_page_frame = property $ do
-    let config = def{CDP.doLogResponses = True}
-    (enabled, nav, numResponses) <- evalIO $ CDP.runClient config $ \handle -> do
-        -- register handler
-        CDP.subscribe handle $ \frameNavigated -> do
-            print $ CDP.pageFrameId . CDP.pageFrameNavigatedFrame $ frameNavigated
-        -- enable events
-        enabled  <- CDP.pageEnable handle
-        -- navigate to page
-        nav     <- CDP.pageNavigate handle $
-            CDP.PPageNavigate "http://wikipedia.com" Nothing Nothing Nothing
-        -- wait for events
-        threadDelay 500000
-        -- check the response buffer
-        responses <- readMVar . R.responseBuffer $ handle
-        pure (enabled, nav, length responses)
+-- Takes too long
+-- prop_page_frame :: Property
+-- prop_page_frame = property $ do
+--     let config = def{CDP.doLogResponses = True}
+--     (enabled, nav, numResponses) <- evalIO $ CDP.runClient config $ \handle@(Handle handle') -> do
+--         -- register handler
+--         CDP.subscribe handle $ \frameNavigated -> do
+--             print $ CDP.pageFrameId . CDP.pageFrameNavigatedFrame $ frameNavigated
+--         -- enable events
+--         enabled  <- CDP.pageEnable handle
+--         -- navigate to page
+--         nav     <- CDP.pageNavigate handle $
+--             CDP.PPageNavigate "http://wikipedia.com" Nothing Nothing Nothing
+--         -- wait for events
+--         threadDelay 2000000
+--         -- check the response buffer
+--         responses <- readMVar . responseBuffer $ handle'
+--         pure (enabled, nav, length responses)
 
-    enabled === Nothing
-    void $ evalEither nav
-    -- check at least 1 event was received, 2 responses are for the commands
-    diff numResponses (>) 2 
+--     enabled === Nothing
+--     void $ evalEither nav
+--     -- check at least 1 event was received, 2 responses are for the commands
+--     diff numResponses (>) 2
 
 main :: IO ()
 main = defaultMain [checkSequential $$(discover)]

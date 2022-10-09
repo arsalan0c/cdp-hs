8ccb2fd69a63d840f172f78105ff71e0242f69ae
Jasper Van der Jeugt <m@jaspervdj.be>
2022-10-09 13:27:34 +0200

    Add example for demo

---
+++ examples/open-twitter.hs
@@ -0,0 +1,20 @@
+module Main where
+
+import CDP
+import Data.Default (def)
+import Control.Concurrent (threadDelay)
+import Control.Monad (forever)
+
+main :: IO ()
+main = CDP.runClient def $ \handle -> forever $ do
+    targetCreateTarget handle PTargetCreateTarget
+        { pTargetCreateTargetUrl = "https://twitter.com/GabriellaG439"
+        , pTargetCreateTargetWidth = Nothing
+        , pTargetCreateTargetHeight = Nothing
+        , pTargetCreateTargetBrowserContextId = Nothing
+        , pTargetCreateTargetEnableBeginFrameControl = Nothing
+        , pTargetCreateTargetNewWindow = Just False
+        , pTargetCreateTargetBackground = Just False
+        }
+
+    threadDelay $ 10 * 1000 * 1000

9492b40caab19c4009580eb8af7b49d9cfb3fe4d
Arsalan Cheema <arsalanc.v2@gmail.com>
2022-10-20 02:38:40 +0800

    fix example

--- examples/subscribe.hs
+++ examples/subscribe.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings   #-}
+
 module Main (main) where
 
 import qualified CDP as CDP
@@ -10,8 +12,8 @@ main :: IO ()
 main = CDP.runClient def $ \handle -> do
     CDP.sendCommandWait handle CDP.pPageEnable
     sub1 <- mfix $ \sub -> CDP.subscribe handle $ \e -> do
-        putStrLn . show $ "1: " ++ CDP.pageFrameUrl (CDP.pageFrameNavigatedFrame e)
+        putStrLn . show $ "1: " <> CDP.pageFrameUrl (CDP.pageFrameNavigatedFrame e)
         CDP.unsubscribe handle sub
     _ <- CDP.subscribe handle $ \e ->
-        putStrLn . show $ "2: " ++ CDP.pageFrameUrl (CDP.pageFrameNavigatedFrame e)
+        putStrLn . show $ "2: " <> CDP.pageFrameUrl (CDP.pageFrameNavigatedFrame e)
     forever $ threadDelay 1000
